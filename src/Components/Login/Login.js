cb09266d46f939d558f28a27029a70a7a09a35d9
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-04-18 15:04:46 +0600

    signup-done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleUserLogin = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+
+    if (user) {
+        navigate('/')
+    }
+
+
+
+
     return (
         <div className="bg-white py-6 sm:py-8 lg:py-12">
             <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
                 <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">Login</h2>
 
-                <form className="max-w-lg border rounded-lg mx-auto">
+                <form onSubmit={handleUserLogin} className="max-w-lg border rounded-lg mx-auto">
                     <div className="flex flex-col gap-4 p-4 md:p-8">
                         <div>
                             <label for="email" className="inline-block text-gray-800 text-sm sm:text-base mb-2">Email</label>
-                            <input name="email" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required type="email" />
+                            <input onBlur={handleEmailBlur} name="email" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required type="email" />
                         </div>
 
                         <div>
                             <label for="password" className="inline-block text-gray-800 text-sm sm:text-base mb-2">Password</label>
-                            <input name="password" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required type="password" />
+                            <input onBlur={handlePasswordBlur} name="password" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required type="password" />
                         </div>
+                        <p>{error?.message}</p>
 
                         <button className="block bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-gray-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Log in</button>
 
